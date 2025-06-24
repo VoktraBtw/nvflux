fdf403683fe610c5c3bc3f6e5010ca70570298f0
VoktraBtw <q787615@gmail.com>
2025-06-25 00:15:12 +0500

    core: clean up unused functions

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -17,27 +17,5 @@ function core.get_os()
   end
 end
 
--- fixes parenthesis issue with directories and telescope
-function core.fix_telescope_parens_win()
-  if vim.fn.has("win32") then
-    local ori_fnameescape = vim.fn.fnameescape
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.fn.fnameescape = function(...)
-      local result = ori_fnameescape(...)
-      return result:gsub("\\", "/")
-    end
-  end
-end
-
-function core.expand_path(path)
-  if path:sub(1, 1) == "~" then
-    return os.getenv("HOME") .. path:sub(2)
-  end
-  return path
-end
-
-function core.center_in(outer, inner)
-  return (outer - inner) / 2
-end
 
 return core
